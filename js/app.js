0a0362b49963728ef695bff15a357c2f417cd64f
Sasha <kremlink0@gmail.com>
2020-06-20 09:25:17 +0300

    wobble upgrade

--- js/app.js
+++ js/app.js
@@ -22,9 +22,13 @@ $(()=>{
 
 /*
 $(()=>{
- let wobble=$('.w-wrap'),
+ let $wobble=$('.w-wrap'),
+  $pr=$('.w-p-text'),
   x=0,
   y=0,
+  prog=5,
+  pCtr=0,
+  timer=39,
   rand=()=>Math.floor(Math.random()*60)-30,
   wTrs=()=>{
    let oX=x,oY=y;
@@ -34,12 +38,22 @@ $(()=>{
 
    if(x===oX&&y===oY)
     wTrs();
-   wobble.css('transform',`translate(${x}px,${y}px)`);
+   $wobble.css('transform',`translate(${x}px,${y}px)`);
   };
 
- $('.w-p div').css({transitionDuration:'5s',width:'100%'});
- wobble.on('transitionend',function(){
+ $('.w-p div').css({transitionDuration:prog+'s',width:'100%'});
+ $wobble.on('transitionend',function(){
   wTrs();
  });
  wTrs();
+
+ let int=setInterval(()=>{
+  let text;
+
+  pCtr++;
+  if(pCtr>prog/timer*1000)
+   clearTimeout(int);
+  text=pCtr*timer/1000;
+  $pr.text(text>prog?prog:text);
+ },timer);
 });*/
